759a5f9d8523cf1745328e93ef89b445300c6b13
Evie0502 <130061176+Evie0502@users.noreply.github.com>
2023-08-20 22:58:48 -0500

    Adds Item config and enables new items

---
+++ src/main/java/KAMKEEL/PluginMod/Config/ConfigItem.java
@@ -0,0 +1,81 @@
+package KAMKEEL.PluginMod.Config;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+import cpw.mods.fml.common.FMLLog;
+import org.apache.logging.log4j.Level;
+
+import java.io.File;
+
+
+public class ConfigItem {
+
+    public static Configuration config;
+
+    public final static String GENERAL = "General";
+
+    /**
+     * General Properties
+     **/
+    public static Property CardsProperty;
+    public static boolean Cards = true;
+
+    public static Property PillsProperty;
+    public static boolean Pills = true;
+
+    public static Property EyesProperty;
+    public static boolean Eyes = true;
+
+    public static Property BallsProperty;
+    public static boolean Balls = true;
+
+    public static Property ArtifactsProperty;
+    public static boolean Artifacts = true;
+
+    public static Property EnergyAttacksProperty;
+    public static boolean EnergyAttacks = true;
+
+    public static Property ApplesProperty;
+    public static boolean Apples = true;
+    public static Property MISCPProperty;
+    public static boolean TreasureMap = true;
+    public static boolean PaperBomb = true;
+    public static boolean MISC = true;
+
+
+    public static void init(File configFile) {
+        config = new Configuration(configFile);
+
+        try {
+            config.load();
+
+            // General
+            CardsProperty = config.get(GENERAL, "Cards", true);
+            Cards = CardsProperty.getBoolean(true);
+
+            PillsProperty = config.get(GENERAL, "Pills", true);
+            Pills = PillsProperty.getBoolean(true);
+
+            EyesProperty = config.get(GENERAL, "Eyes", true);
+            Eyes = EyesProperty.getBoolean(true);
+
+            BallsProperty = config.get(GENERAL, "Balls", true);
+            Balls = BallsProperty.getBoolean(true);
+
+            ArtifactsProperty = config.get(GENERAL, "Artifacts", true);
+            Artifacts = ArtifactsProperty.getBoolean(true);
+
+            EnergyAttacksProperty = config.get(GENERAL, "EnergyAttacks", true);
+            EnergyAttacks = EnergyAttacksProperty.getBoolean(true);
+
+        } catch (Exception e) {
+            FMLLog.log(Level.ERROR, e, "PluginMod has had a problem loading its main configuration");
+        } finally {
+            if (config.hasChanged()) {
+                config.save();
+            }
+        }
+    }
+}
+
+
--- src/main/java/KAMKEEL/PluginMod/Config/LoadConfiguration.java
+++ src/main/java/KAMKEEL/PluginMod/Config/LoadConfiguration.java
@@ -6,12 +6,17 @@ public class LoadConfiguration {
     public static File blocksConfig;
     public static File compatConfig;
 
+    public static File compatItem;
+
     public static void init(String configpath)
     {
         blocksConfig = new File(configpath + "blocks.cfg");
         compatConfig = new File(configpath + "compat.cfg");
+        compatItem = new File(configpath + "Item.cfg");
+
 
         ConfigBlocks.init(blocksConfig);
         ConfigCompat.init(compatConfig);
+        ConfigItem.init(compatItem);
     }
 }
